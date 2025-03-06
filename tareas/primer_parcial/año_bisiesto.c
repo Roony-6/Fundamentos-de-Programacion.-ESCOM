ff27398c7f99e704098d522f81bd7f6bc53a1372
Roony-6 <roony7.rrc@gmail.com>
2025-03-05 18:59:46 -0600

    leap year

--- tareas/primer_parcial/año_bisiesto.c
+++ tareas/primer_parcial/año_bisiesto.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
 int main(){//main funtion
-    int y;//declaration of a variable (year)
+    int year;//declaration of a variable (year)
     printf("give me a year: ");
-    scanf("%d",&y);//reading the calue of the year
-    if ((y%4==0 && y%100!=0) || y%400==0){//condition to know if the year is a leap year
+    scanf("%d",&year);//reading the calue of the year
+    if ((year%4==0 && year%100!=0) || year%400==0){//condition to know if the year is a leap year
 
-        printf("%d is a leap year ",y);
+        printf("%d is a leap year ",year);
 
     }else{
-        printf("%d is not a leap year ",y);
+        printf("%d is not a leap year ",year);
     }
     return 0;//end
 }
