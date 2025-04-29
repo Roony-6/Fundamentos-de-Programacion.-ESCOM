c2941fd04c43e5d083ae0c2a076feeec76e34eaf
Roony-6 <roony7.rrc@gmail.com>
2025-04-29 07:28:50 -0600

    ejercicios de logigo con bucles

--- clase/primer_parcial/bucles/ejercicioLogica.c
+++ clase/primer_parcial/bucles/ejercicioLogica.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-
+void Secuencia(int);
 int main(){
     int n;
     printf("dame el valor de n\n");
     scanf("%d",&n);
-    secuencia(n);
+    Secuencia(n);
 
 }
 
-void secuencia(int n){
+void Secuencia(int n){
     for(int i=1;i<=n;i++){
         for (int j=0;j<n;j++){
             printf("%d",i);
