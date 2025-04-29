cca90d886529ba75d1ba2326a685d274159fcf3c
Roony-6 <roony7.rrc@gmail.com>
2025-04-29 07:56:14 -0600

    ejercicios

--- clase/primer_parcial/bucles/ejercicioLogica.c
+++ clase/primer_parcial/bucles/ejercicioLogica.c
@@ -5,12 +5,24 @@ int main(){
     printf("dame el valor de n\n");
     scanf("%d",&n);
     Secuencia(n);
+    puts("----------------------");
+    Secuencia2(n);
 
+    return 0;
 }
 
 void Secuencia(int n){
     for(int i=1;i<=n;i++){
         for (int j=0;j<n;j++){
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+}
+
+void Secuencia2(int n){
+    for(int i=1;i<=n;i++){
+        for (int j=0;j<i;j++){
             printf("%d",i);
         }
         printf("\n");
