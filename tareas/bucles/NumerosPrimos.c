1e13fb4ee01f847f21626293c2c9d500116112fc
Roony-6 <roony7.rrc@gmail.com>
2025-04-30 07:39:45 -0600

    otra manera de hacerlo

--- tareas/bucles/NumerosPrimos.c
+++ tareas/bucles/NumerosPrimos.c
@@ -11,6 +11,11 @@ int main() {
         printf("%d no es un número primo.\n", numero);
     }
 
+    if (esPrimo2(numero)) {
+        printf("%d es un número primo.\n", numero);
+    } else {
+        printf("%d no es un número primo.\n", numero);
+    }
     return 0;
 }
 
@@ -24,4 +29,23 @@ int esPrimo(int n) {
         }
     }
     return 1; // Es primo
+}
+
+//otra forma de hacerlo:
+
+int esPrimo2(int n){
+    int i=2;
+    if(n<=1){
+        return 0;
+    }
+    if(n==2) return 1;
+    while((n%i!=0)&& (i*2<n)){
+        i++;
+    }
+    if(i*2>n){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
