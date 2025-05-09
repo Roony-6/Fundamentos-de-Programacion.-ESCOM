2f56104bbd3efaa5acfb2bf7c765835659898813
Roony-6 <roony7.rrc@gmail.com>
2025-05-09 07:38:20 -0600

    Ejercicio Matriz

--- clase/matrices/ejreciciosMatrices.c
+++ clase/matrices/ejreciciosMatrices.c
@@ -3,7 +3,7 @@
 void llenarMatriz(int [][100],int);
 int main(){
 int matriz[100][100],n;
-printf("Dame la long de la matriz");
+printf("Dame la long de la matriz\n");
 scanf("%d",&n);
 llenarMatriz(matriz,n);
 imprimirMatrix(matriz,n);
@@ -13,14 +13,14 @@ imprimirMatrix(matriz,n);
 void llenarMatriz(int matriz[][100], int n){
  for (int i=0;i<n;i++){
     for(int j=0;j<n;j++){
-        matriz[i][j]=i;
+        matriz[i][j]=i+1;
     }
  }
 }
 void imprimirMatrix(int matriz[][100], int n){
     for (int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            printf("%d",matriz[i][j]+1);
+            printf("%d",matriz[i][j]);
         }
         putchar('\n');
      }
