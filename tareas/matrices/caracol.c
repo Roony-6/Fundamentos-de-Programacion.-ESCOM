0880ffa29c12c9ee89f869db6fd1c1d53ca8c8ac
Roony-6 <roony7.rrc@gmail.com>
2025-06-10 07:20:00 -0600

    caracol

--- tareas/matrices/caracol.c
+++ tareas/matrices/caracol.c
@@ -1,60 +1,73 @@
 #include<stdio.h>
-void caracol(int [][500], int);
-int main(){
 
-    int matriz[][500]={0};
+// Prototipo de la función que llena la matriz en forma de caracol
+void caracol(int [][500], int);
+
+// Función principal
+int main(){
+    // Declaración de la matriz y la variable para el tamaño
+    int matriz[500][500] = {0}; // Matriz inicializada en 0
     int n;
 
+    // Solicita al usuario la longitud de la matriz
     printf("dame el la long de la matriz");
     scanf("%d",&n);
 
-    caracol(matriz,n);
-    imprimirMatriz(matriz,n);
+    // Llama a la función para llenar la matriz en forma de caracol
+    caracol(matriz, n);
+
+    // Imprime la matriz resultante
+    imprimirMatriz(matriz, n);
 
     return 0;
 }
 
-void imprimirMatriz(int matriz[][500],int n){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            printf("%d ",matriz[i][j]);
+// Función que imprime una matriz cuadrada de tamaño n x n
+void imprimirMatriz(int matriz[][500], int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ", matriz[i][j]);
         }
         putchar('\n');
     }
 }
 
+// Función que llena una matriz de tamaño n x n en forma de caracol (espiral)
+// m: matriz a llenar
+// n: tamaño de la matriz (n x n)
 void caracol(int m[][500], int n) {
-    int f_start = 0, f_end = n - 1; // Row boundaries
-    int c_start = 0, c_end = n - 1; // Column boundaries
-    int num = 1; // Number to fill in the matrix
+    int f_start = 0, f_end = n - 1; // Límites de filas
+    int c_start = 0, c_end = n - 1; // Límites de columnas
+    int num = 1; // Número a colocar en la matriz
 
+    // Mientras queden capas por llenar
     while (f_start <= f_end && c_start <= c_end) {
-        // Fill top row (left to right)
+        // Llena la fila superior de izquierda a derecha
         for (int c = c_start; c <= c_end; c++) {
             m[f_start][c] = num++;
         }
-        f_start++; // Move the top boundary down
+        f_start++; // Mueve el límite superior hacia abajo
 
-        // Fill right column (top to bottom)
+        // Llena la columna derecha de arriba a abajo
         for (int f = f_start; f <= f_end; f++) {
             m[f][c_end] = num++;
         }
-        c_end--; // Move the right boundary left
+        c_end--; // Mueve el límite derecho hacia la izquierda
 
-        // Fill bottom row (right to left), if applicable
+        // Llena la fila inferior de derecha a izquierda, si corresponde
         if (f_start <= f_end) {
             for (int c = c_end; c >= c_start; c--) {
                 m[f_end][c] = num++;
             }
-            f_end--; // Move the bottom boundary up
+            f_end--; // Mueve el límite inferior hacia arriba
         }
 
-        // Fill left column (bottom to top), if applicable
+        // Llena la columna izquierda de abajo hacia arriba, si corresponde
         if (c_start <= c_end) {
             for (int f = f_end; f >= f_start; f--) {
                 m[f][c_start] = num++;
             }
-            c_start++; // Move the left boundary right
+            c_start++; // Mueve el límite izquierdo hacia la derecha
         }
     }
 }
