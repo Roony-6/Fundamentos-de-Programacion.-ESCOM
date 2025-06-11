6fc684dcc648f977ffdfa5754eb934dc52e4737f
Roony-6 <roony7.rrc@gmail.com>
2025-06-10 20:01:08 -0600

    Ordenamiento por insercion

--- clase/bucles/insercion.c
+++ clase/bucles/insercion.c
@@ -1,52 +1,54 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-void ordenarInsercion(int [],int);
-void intercambia(int *, int *);
-void LlenaArrIntAleaIter(int[],int);
-void ImpArrInt(int[],int);
+
+// Prototipos de funciones
+void ordenarInsercion(int [],int);      // Ordena un arreglo usando el método de inserción
+void intercambia(int *, int *);         // Intercambia dos valores enteros
+void LlenaArrIntAleaIter(int[],int);   // Llena un arreglo con números aleatorios
+void ImpArrInt(int[],int);              // Imprime los elementos de un arreglo
+
 int main(){
-    srand(time(NULL));
-    int array[100],n;
+    srand(time(NULL)); // Inicializa la semilla para números aleatorios
+    int array[100], n;
     printf("dame el numero de elementos\n");
     scanf("%d",&n);
-    LlenaArrIntAleaIter(array,n);
-    ImpArrInt(array,n);
-    ordenarInsercion(array,n);
-    ImpArrInt(array,n); 
+    LlenaArrIntAleaIter(array, n); // Llena el arreglo con valores aleatorios
+    ImpArrInt(array, n);           // Imprime el arreglo original
+    ordenarInsercion(array, n);    // Ordena el arreglo por inserción
+    ImpArrInt(array, n);           // Imprime el arreglo ordenado
 
     return 0;
 }
 
 void ordenarInsercion(int array[], int n){
-
-    int i=1, j;
-    while (i<n)
-    {
-        j=i;
-        while (j>0 && array[j]<array[j-1])
-        {
-            intercambia(array+j,array+j-1);
+    int i = 1, j;
+    while (i < n) {
+        j = i;
+        // Desplaza el elemento hacia la izquierda hasta que esté en la posición correcta
+        while (j > 0 && array[j] < array[j-1]) {
+            intercambia(array + j, array + j - 1);
             j--;
         }   
         i++;
     }
 }
-void intercambia(int *a, int *b){
-    int t=*a;
-    *a=*b;
-    *b=t;
-}
- void ImpArrInt(int a[], int n){
-    int i;
-    for(i=0;i<n;i++)
-        printf("a[%d]=%d\t",i,a[i]);
-    putchar('\n');
- }
 
- 
- void LlenaArrIntAleaIter(int a[],int n){
-     int i;
-     for(i=0;i<n;i++)
-        a[i]=1+rand()%100;
-    };
+void intercambia(int *a, int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void ImpArrInt(int a[], int n){
+    int i;
+    for(i = 0; i < n; i++)
+        printf("a[%d]=%d\t", i, a[i]);
+    putchar('\n');
+}
+
+void LlenaArrIntAleaIter(int a[], int n){
+    int i;
+    for(i = 0; i < n; i++)
+        a[i] = 1 + rand() % 100;
+}
