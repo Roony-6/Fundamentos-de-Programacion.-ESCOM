ec0214b396e1aa3d1c78c51692e0c6efd7fee0e7
Roony-6 <roony7.rrc@gmail.com>
2025-06-13 07:50:12 -0600

    Uniones en c

--- clase/Strucs/uniones.c
+++ clase/Strucs/uniones.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // Define a union with 
 // different data types
@@ -6,12 +7,20 @@ union Student {
     int rollNo;
     float height;
     char firstLetter;
+    char name[80];
+};
+struct Student2 {
+    int rollNo;
+    float height;
+    char firstLetter;
+    char name[80];
 };
 
 int main() {
     
     // Declare a union variable
     union Student data;
+    struct Student2 data2;
 
     // Assign and print the roll number
     data.rollNo = 21;
@@ -20,7 +29,11 @@ int main() {
     printf("%.2f\n", data.height);
     data.firstLetter = 'N';
     printf("%c\n", data.firstLetter);
-    printf("%zu\n", sizeof(data));
 
+    strcpy(data.name, "Roony");
+    printf("%s\n", data.name);
+    printf("%zu\n", sizeof(data));
+    printf("%zu\n", sizeof(data2));
     return 0;
+
 }
